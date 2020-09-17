5d56754f49924504028c34136a8c558c807c6d80
hachi <eojin312@naver.com>
2020-09-17 16:04:42 +0900

    lesson3 - PermMissingElem

---
+++ src/test/java/hachi/algorithmstudy/lesson3/PermMissingElemTest.java
@@ -0,0 +1,24 @@
+package hachi.algorithmstudy.lesson3;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PermMissingElemTest {
+    public int solution(int[] A) {
+        Arrays.sort(A);
+        for (int i = 0; i < A.length; i++) {
+            if (i + 1 != A[i]) {
+                return i + 1;
+            }
+        }
+        return A.length + 1;
+    }
+
+    @Test
+    void solution() {
+        solution();
+    }
+}
