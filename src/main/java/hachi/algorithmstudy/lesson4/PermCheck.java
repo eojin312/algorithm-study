487b6d9c4e7cff468d4a29062dd7fa0ffd16033e
hachi <eojin312@naver.com>
2020-09-22 22:02:59 +0900

    공백제거

--- src/main/java/hachi/algorithmstudy/lesson4/PermCheck.java
+++ src/main/java/hachi/algorithmstudy/lesson4/PermCheck.java
@@ -4,10 +4,7 @@ import java.util.Arrays;
 
 public class PermCheck {
     public int solution(int[] A) {
-
         Arrays.sort(A);
-
-
         final int ALength = A.length;
         for (int i = 1; i <= ALength; i++) {
             if (A[i-1] != i) return 0; // is not a permutation
