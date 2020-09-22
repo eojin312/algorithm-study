ee81d9a4a2b413d25fb33b28563bf57a9b822c77
hachi <eojin312@naver.com>
2020-09-22 22:57:16 +0900

    lesson5- MinAvgTwoSliceTest 보강

--- src/test/java/hachi/algorithmstudy/lesson5/MinAvgTwoSliceTest.java
+++ src/test/java/hachi/algorithmstudy/lesson5/MinAvgTwoSliceTest.java
@@ -1,5 +1,6 @@
 package hachi.algorithmstudy.lesson5;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class MinAvgTwoSliceTest {
@@ -10,11 +11,14 @@ class MinAvgTwoSliceTest {
 
         int[] A = {4, 2, 2, 5, 1, 5, 8};
         int result = minAvgTwoSlice.solution(A);
+        System.out.println(result);
+        Assertions.assertEquals(1, minAvgTwoSlice.solution(A));
 
 
         int[] B = {-3, -5, -8, -4, -10};
         int result2 = minAvgTwoSlice.solution(B);
         System.out.println(result2);
+        Assertions.assertEquals(2, minAvgTwoSlice.solution(B));
     }
 
     @Test
@@ -23,7 +27,6 @@ class MinAvgTwoSliceTest {
 
         int[] A = {4, 2, 2, 5, 1, 5, 8};
         int result = minAvgTwoSlice.solution(A);
-
         System.out.println(result);
     }
 
